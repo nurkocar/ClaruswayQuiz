88ecb5d9849c6cbbbf10f822da4e28cb8e747239
Nur Kocar <64444237+nurkocar@users.noreply.github.com>
2021-01-14 18:51:59 +0000

    first commit

---
+++ src/quiz/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+
+# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        verbose_name_plural = 'Categories'
+        
+        
+class Quiz(models.Model):
+    title = models.CharField(max_length=100, verbose_name='Quiz title')
+    category = models.ForeignKey(Category, on_delete = models.CASCADE)
+    date_created = models.DateTimeField(auto_now_add=True)
+    
+    
+    def __str__(self):
+        return self.title
+    
+    class Meta:
+        verbose_name_plural = 'Quizzes'
+        
+        
+class Question(models.Model):
+    quiz = models.ForeignKey(Quiz, on_delete=models.CASCADE)
+    title = models.CharField(max_length=300, verbose_name='Question Title')
